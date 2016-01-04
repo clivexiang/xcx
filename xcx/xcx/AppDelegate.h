6b4ed88e19d728d1019e0f696aa229fd9e520404
clive <1091673851@qq.com>
2016-01-04 15:50:31 +0800

    init project

---
+++ xcx/xcx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcx
+//
+//  Created by mac on 16/1/4.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcx/xcx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcx
+//
+//  Created by mac on 16/1/4.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
